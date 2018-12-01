702a7a6244f681b67b9b4ad2c12d028a56df6c9b
Autumn <autumncbullard@gmail.com>
2018-11-30 22:19:09 -0500

    Create SVG where chart will be displayed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 function displayGraph() {
 
   axios.get('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json').then((dataset) => {
+    const w = 1000;
+    const h = 500;
+    const svg = d3.select('.chart')
+      .append('svg')
+      .attr('width', w)
+      .attr('height', h);
   }).catch(() => {
   });
 }
